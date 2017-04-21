79a83fe4b43de0a5609a05d164e37e3e4436f70c
Kelly Miklas <kmiklas@umich.edu>
2017-04-21 16:26:00 -0400

    movemenet but not compiling

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+	public float strafingSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movement ();
+	}
+
+	// Player movement –> only will move along X axis with A + D
+	void movement() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		// (x, y, z)
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		rigidbody.velocity = movement;
+
+
+	}
+}
